46364c8b7abcaa6c3ef3e26b38cccc12b8a5790c
Massoud Safari <massoud.safari@outlook.com>
2024-02-04 21:04:18 +0100

    Add project files.

---
+++ OnlineStore.Domain/Entities/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Domain.Entities
+{
+    public sealed class Order
+    {
+        public long Id { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public int BuyerId { get; set; }
+        public User Buyer { get; set; }
+    }
+}
---
+++ OnlineStore.Domain/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Domain.Entities
+{
+    public sealed class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(maximumLength: 40)]
+        public string Title { get; set; }
+        public int InventoryCount { get; set; }
+        public decimal Price { get; set; }
+        public double Discount { get; set; }
+    }
+}
